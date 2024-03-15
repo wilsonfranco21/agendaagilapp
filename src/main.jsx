a0e00937d2961f5f96b0802118af3d7da393b958
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-03-15 17:32:51 -0500

    secrea componente home

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import "bootstrap/dist/css/bootstrap.min.css" // utilizar bossttrap despues de instlarlo
+import "bootstrap/dist/js/bootstrap.min.js"
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  
+  </React.StrictMode>,
+)
